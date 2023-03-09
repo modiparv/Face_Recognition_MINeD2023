af4a22c65d1f8cbc3381bd7f13a9f6b0924152e6
Parv Modi <93371014+modiparv@users.noreply.github.com>
2023-03-09 23:05:21 +0530

    Add files via upload

---
+++ read_db.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from PIL import Image
+import io
+from bson.binary import Binary
+from PIL import Image
+import io
+import matplotlib.pyplot as plt
+
+client = MongoClient(
+    'mongodb+srv://user-4:user-4pass@cluster1.h9nlbyo.mongodb.net/test')
+db = client['employee_jk']
+images = db.images
+
+image_bytes = io.BytesIO()
+
+image = images.find_one({
+    'id': 2031
+})
+print(image['id'])
+pil_img = Image.open(io.BytesIO(image['data']))
+plt.imshow(pil_img)
+plt.show()
+
+for i in image:
+    print(i['id'])
+    pil_img = Image.open(io.BytesIO(i['data']))
+    plt.imshow(pil_img)
+    plt.show()
